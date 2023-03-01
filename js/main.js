520903990acba3e0831315434c12ed25bdb37774
Yehor Babych <ebabich0703@gmail.com>
2023-03-01 18:42:23 +0100

    test commit 1

---
+++ js/main.js
@@ -0,0 +1,16 @@
+function trans_ua(){
+    document.getElementById('main-name-1').innerHTML = "Розплідник";
+    document.getElementById('main-name-3').innerHTML = "Померанські шпиці";
+    
+    document.getElementById('block-2-title').innerHTML = "Чим виділяються шпиці?";
+    document.getElementById('block-2-text').innerHTML = " <p>&nbsp Померанці - дуже веселі та активні собаки.   Вони відрізняються своїм добрим характером та вірністю господарю, легко піддаються дресируванню. Шпиці люблять увагу та догляд за собою. </p><p> &nbsp Як i всі собаки, померанські шпиці потребують прогулянок, але їх васнику потрібну бути обережним. Померанці, не дивлячись на свій маленький розмір, можуть задирати інших собак. У їх малому тілі знаходиться серце справжнього лева.</p> <p> &nbsp Тим не менш, вони спокійно живуть під одним дахом із іншими тваринами. Також померанці можуть спокійно “ходити” на пелюшки. </p> <p> &nbsp В кінці-кінців немає нічого прекраснішого за таку меленьку грудочку щастя та милоти.</p>";
+
+    document.getElementById('title-2').innerHTML = "ЧОМУ САМЕ МИ";
+    document.getElementById('column-1').innerHTML = "<p class='text'>&nbsp Ми займаємося розплідненням собак вже 10 років і знаємося на цій справі. Ми слідкуємо за відповідністю наших собак стандартам породи та вибираємо найкращих кобелів для в’язки.</p> <p class='text'>&nbsp Наші собаки принесуть до Вашого дому любов та щастя.</p> <p class='text'>&nbsp Ми вкладаємо у наш розплідник душу. Усі собаки отримають належний догляд та любов. </p>";
+    document.getElementById('column-2').innerHTML = "<p class='text'> &nbspМи відповідально ставимося до пошуку нових хазяїв для наших цуценят. Ми намагаємося знайти добрі сім’ї, які точно будь любити свого улюбленця та піклуватися про нього.</p><p class='text'> &nbsp Важливим аспектом у виборі собаки є його й зовнішній вигляд. Нажаль, ніхто не може прогнозувати, яким народиться щеня, проте ви можете побачити наших вже народившихся цуценят <a href='../html/our-dogs.html' id='block-4-link' target='_blank'>тут</a>.</p>";
+
+    document.getElementById('footer-1').innerHTML = "<h6 class='s-title' id='footer-title-1'>Подяки</h6><p class='s-text'>Ми хотіли би виразити подяку усім хто нас підтримує та купує наших собак. Окремо дякуємо, розпліднику Elen house. </p><p class='s-text'>Ми дуже вдячні платформі <a href='https://freeicons.io/' target='_blank' class='f-links'>Freeicons.io</a>, <a href='https://freeicons.io/profile/3' target='_blank' class='f-links'>@icon king1</a>, за надання нам іконок для створення сайту, та фотографу @nataly.mazulevska.photo, за її чудові фотографії. </p><p class='s-text'>Дякуємо, за те що ви є!</p>";
+    document.getElementById('footer-2').innerHTML = " <h6 class='s-title' id='footer-title-2'>Навігація</h6><div><a class='nav-links-s s-text' id='main-page' href='../html/index.html'>Головна</a><a class='nav-links-s s-text' href='../html/our-dogs.html' target='_blank'>Наші собаки</a><a class='nav-links-s s-text' href='../html/our-history.html' target='_blank'>Наша історія</a></div><h6 class='s-title' id='footer-con'>Наші контакти</h6><div class='s-text'>тел.: +380-67-290-20-31;+ 48-787-390-683</div>";   
+}
+
+              
